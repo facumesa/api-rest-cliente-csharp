91a26f682a708cd7f7b1a4093257508defa69bd1
Facundo Mesa Babon <FM359734@fi365.ort.edu.uy>
2026-04-16 13:50:56 -0300

    Config de EF, Repo a BD pendiente

--- ObligatorioDW2026/Negocio/ValueObjects/Password.cs
+++ ObligatorioDW2026/Negocio/ValueObjects/Password.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Negocio.ValueObjects
 {
+    [ComplexType]
     public class Password
     {
         public string Valor { get; set; }
