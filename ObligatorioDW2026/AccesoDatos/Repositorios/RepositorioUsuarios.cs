fb9485cbaf9af54e965af69e101bb0584eea8170
Facundo Mesa Babon <FM359734@fi365.ort.edu.uy>
2026-04-20 08:58:54 -0300

    Configuracion de BD para persistencia

--- ObligatorioDW2026/AccesoDatos/Repositorios/RepositorioUsuarios.cs
+++ ObligatorioDW2026/AccesoDatos/Repositorios/RepositorioUsuarios.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AccesoDatos.EF;
 using Negocio.Dominio;
 using Negocio.InterfacesRepo;
 using Negocio.ValueObjects;
@@ -9,37 +10,38 @@ namespace AccesoDatos.Repositorios
 {
     public class RepositorioUsuarios : IRepositorioUsuarios
     {
-        //Cambiar repo a BD y probar
         private static int ultId = 0;
-        private static List<Usuario> usuarios = new List<Usuario>();
-        public RepositorioUsuarios()
+        public StellarContext Contexto { get; set; }
+
+        public RepositorioUsuarios(StellarContext contexto)
         {
-            if (usuarios.Count == 0)
-            {
-                this.PrecargarUsuarios(); 
-            }
+            Contexto = contexto;
         }
+
         public void Add(Usuario nuevo)
         {
             nuevo.Validar();
             nuevo.Id = ultId++;
-            usuarios.Add(nuevo);
+            Contexto.Usuarios.Add(nuevo);
+            Contexto.SaveChanges();
+            
         }
 
         public IEnumerable<Usuario> FindAll()
         {
-            return usuarios;
+            return Contexto.Usuarios.ToList();
         }
 
         public Usuario FindById(int id)
         {
-            throw new NotImplementedException();
+            return Contexto.Usuarios.Find(id);
         }
 
         public IEnumerable<Socio> GetSocios()
         {
+            //SOLO MODO DE PRUEBA, SE REMPLAZARA CON LINQ
             List<Socio> socios = new List<Socio>();
-            foreach (Usuario u in usuarios)
+            foreach (Usuario u in Contexto.Usuarios.ToList())
             {
                 if (u is Socio s)
                 {
@@ -51,7 +53,8 @@ namespace AccesoDatos.Repositorios
 
         public Usuario Login(string nombreUsuario, string password)
         {
-            foreach (var usuario in usuarios)
+            //SOLO MODO DE PRUEBA, SE REMPLAZARA CON LINQ
+            foreach (var usuario in Contexto.Usuarios.ToList())
             {
                 if (usuario.NombreUsuario == nombreUsuario && usuario.Contrasenia.Valor == password) return usuario;
             }
@@ -67,37 +70,6 @@ namespace AccesoDatos.Repositorios
         public void Update(Usuario nuevo)
         {
             throw new NotImplementedException();
-        }
-
-        public void PrecargarUsuarios()
-        {
-            Administrador admin = new Administrador(
-                    "Facundo Mesa",
-                    "Ciudad de la Costa",
-                    "099123456",
-                    new Email("admin@ort.edu.uy"),
-                    "admin",
-                    new Password("Admin123!")
-                );
-            Socio socio = new Socio(
-                    "Oriana Rodriguez",
-                    "Montevideo",
-                    "098765432",
-                    new Email("ori@gmail.com"),
-                    "ori_socia",
-                    new Password("Socio123!")
-                );
-            Socio socio2 = new Socio(
-                    "Juan Perez",
-                    "Piriápolis",
-                    "091111222",
-                    new Email("juan@perez.com"),
-                    "juanp",
-                    new Password("Juanp123!")
-                );
-            this.Add(admin);
-            this.Add(socio);
-            this.Add(socio2);
         }
     }
 }
