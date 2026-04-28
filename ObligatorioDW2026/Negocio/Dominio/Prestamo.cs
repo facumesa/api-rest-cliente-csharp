ed46ff4289bc90bcf8ea1c280d3a73de2ab9edcc
Facundo Mesa Babon <FM359734@fi365.ort.edu.uy>
2026-04-28 11:04:48 -0300

    Se agrega la clase Prestamo y su repositorio (Vinculado al CRUD de Equipos por la baja de los mismos)

--- ObligatorioDW2026/Negocio/Dominio/Prestamo.cs
+++ ObligatorioDW2026/Negocio/Dominio/Prestamo.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using Negocio.InterfacesDominio;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Excepciones.ExcepcionesPropias;
 
 namespace Negocio.Dominio
 {
-    public class Prestamo
+    public class Prestamo : IValidable
     {
+        public int Id { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public EstadoPrestamo Estado { get; set; }
+        public Socio Socio { get; set; }
+        public Coordinador Coordinador{ get; set; }
+        public Montura Montura { get; set; }
+        public Telescopio Telescopio { get; set; }
+        public Camara? Camara { get; set; }
+        public Ocular? Ocular { get; set; }
+
+        public void Validar()
+        {
+            if (FechaFin < FechaInicio)
+                throw new OperacionInvalidaException("La fecha de fin no puede ser previa al inicio.");
+
+            if (Telescopio == null || Montura == null)
+                throw new OperacionInvalidaException("El telescopio y la montura son obligatorios.");
+
+            if (Camara == null && Ocular == null)
+                throw new OperacionInvalidaException("Debe solicitar al menos una cámara o un ocular.");
+
+            if (Montura.CargaUtil_kg < Telescopio.Peso)
+                throw new OperacionInvalidaException("La montura no soporta el peso del telescopio.");
+
+            if (Camara != null && Montura.Tipo == TipoMontura.Alt_Azimutal)
+                throw new OperacionInvalidaException("Para astrofotografía se requiere montura Ecuatorial o Híbrida.");
+        }
     }
-}
+ }
+
--- ObligatorioDW2026/Negocio/obj/Debug/net10.0/Negocio.AssemblyInfo.cs
+++ ObligatorioDW2026/Negocio/obj/Debug/net10.0/Negocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b45c5f796511f9c6e4b6abad347a2303178bd9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8043fd728b3b051075d8b49e855e67fbe4c419b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
