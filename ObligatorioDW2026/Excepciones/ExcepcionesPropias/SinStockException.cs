d021866b73835b955635494e60942ea14024e4ab
Facundo Mesa Babon <FM359734@fi365.ort.edu.uy>
2026-04-30 13:08:12 -0300

    Validacion stock al hacer prestamo

---
+++ ObligatorioDW2026/Excepciones/ExcepcionesPropias/SinStockException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excepciones.ExcepcionesPropias
+{
+    public class SinStockException : Exception
+    {
+        public SinStockException()
+        {
+        }
+
+        public SinStockException(string? message) : base(message)
+        {
+        }
+
+        public SinStockException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
